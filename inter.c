5b3867a6ca5cca1bfd066b39309845e99670df4c
Kevyn Catrix <kcatrix@c2r6p3.42nice.fr>
2021-12-06 15:16:41 +0100

    first commit

--- inter.c
+++ inter.c
@@ -6,23 +6,60 @@
 /*   By: kcatrix <kcatrix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 13:34:14 by kcatrix           #+#    #+#             */
-/*   Updated: 2021/12/06 14:02:53 by kcatrix          ###   ########.fr       */
+/*   Updated: 2021/12/06 15:12:04 by kcatrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void	ft_inter(char *str, char *str2)
+int	strverif(char *strv, char no)
 {
 	int i;
 
 	i = 0;
-	while (str[i])
+	while(strv[i])
 	{
-		write(1,&str[i],1);
+		if(strv[i] == no)
+			return (0);
 		i++;
 	}
+	return(1);
+}
+
+void	ft_inter(char *str, char *str2)
+{
+	int i;
+	int y;
+	int z;
+	char *res;
+
+	i = 0;
+	y = 0;
+	z = 0;
+
+	while(str[i])
+	{
+		while(str2[y])
+		{
+			if (str[i] == str2[y] && (strverif(res, str2[y]) == 1))
+			{
+				res[z] =  str2[y];
+				z++;
+			}
+			y++;
+		}
+		i++;
+		y = 0;
+	}
+	res[z] = '\0';
+	i = 0;
+	while(res[i])
+	{
+		write(1, &res[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
 }
 int main(int argc, char **argv)
 {
